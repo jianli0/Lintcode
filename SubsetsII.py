512f301d31a3184a51f602498d184f0025710f1b
xljsskk <jli@ccs.neu.edu>
2015-08-29 12:08:28 -0400

    Done Subset II

--- SubsetsII.py
+++ SubsetsII.py
@@ -1,11 +1,11 @@
-# time complexity : O(2**n)
+# remove the duplicates and do subset
 
 class Solution:
     """
     @param S: The set of numbers.
     @return: A list of lists. See example.
     """
-    def subsets(self, S):
+    def subsetsWithDup(self, S):
         result = []
 
         if S == None or not len(S):
@@ -24,6 +24,11 @@ class Solution:
         # print "result is now %r" %result
 
         for i in range(pos,len(S)):
+            if (i != pos and S[i] == S[i - 1]):
+                # debug
+                # print "pos is %d" %i
+                continue
+
             current.append(S[i])
             # debug
             # print "current is now %r" %current
@@ -32,15 +37,25 @@ class Solution:
 
 #test
 if __name__ == '__main__':
-    S1 = []
-    S2 = None
-    S3 = [1,2,3]
+    # S1 = []
+    # S2 = None
+    # S3 = [1,2,3]
+    S4 = [1, 2, 2]
+    S5 = [0]
+    S6 = [1, 1]
+
     a = Solution()
+    # print "-"*20
+    # print a.subsetsWithDup(S1)
+    # print "-"*20
+    # print a.subsetsWithDup(S2)
+    # print "-"*20
+    # print a.subsetsWithDup(S3)
     print "-"*20
-    print a.subsets(S1)
+    print a.subsetsWithDup(S4)
     print "-"*20
-    print a.subsets(S2)
+    print a.subsetsWithDup(S5)
     print "-"*20
-    print a.subsets(S3)
+    print a.subsetsWithDup(S6)
 
 
