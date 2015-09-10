83d0ee090c17595e5583495febd49ec64ffc00fd
xljsskk <jli@ccs.neu.edu>
2015-09-10 10:04:39 -0400

    Done First Bad Version

---
+++ First Bad Version.py
@@ -0,0 +1,32 @@
+#class VersionControl:
+#    @classmethod
+#    def isBadVersion(cls, id)
+#        # Run unit tests to check whether verison `id` is a bad version
+#        # return true if unit tests passed else false.
+# You can use VersionControl.isBadVersion(10) to check whether version 10 is a
+# bad version.
+
+class Solution:
+    """
+    @param n: An integers.
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        # write your code here
+        start, end = 1, n
+        while start + 1 < end:
+            mid = (start + end) / 2
+            if VersionControl.isBadVersion(mid):
+                end = mid
+            else:
+                start = mid
+
+        if VersionControl.isBadVersion(start):
+            return start
+        return end
+
+
+
+
+
+
